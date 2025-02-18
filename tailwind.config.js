c3dc112c96da2e682138a25c67d119e6fea59ddc
Soliam006 <wsottoja7@alumnes.ub.edu>
2025-02-18 21:28:02 +0100

    TW_problem

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,tsx,jsx}", // Ajusta esto según tu proyecto
+    "./components/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
